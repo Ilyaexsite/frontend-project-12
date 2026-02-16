c4c6229e14238e4339a3043f0b1570d17c625af5
Ilyaexsite <ilyatsarygin64@gmail.com>
2026-02-16 20:47:13 +0300

    push

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5001;
 
+// Раздаем статические файлы
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
 });
 
